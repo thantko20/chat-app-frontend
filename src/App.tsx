d89de20ceeeda2908b98137d9917bbcf5da033a8
Thant Ko Zaw <thantmkou10@gmail.com>
2022-11-20 18:37:11 +0630

    Fix Conditional routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,49 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from './features/auth/AuthProvider';
 
-const App = () => {
-  const { isCheckingUser } = useAuth();
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/' className='text-blue-500 underline cursor-pointer'>
+            Main
+          </Link>
+        </li>
+        <li>
+          <Link
+            to='/friends'
+            className='text-blue-500 underline cursor-pointer'
+          >
+            Friends
+          </Link>
+        </li>
+        <li>
+          <Link
+            to='/auth/login'
+            className='text-blue-500 underline cursor-pointer'
+          >
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link
+            to='/auth/register'
+            className='text-blue-500 underline cursor-pointer'
+          >
+            Register
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
+const App = () => {
   return (
     <div>
-      {!isCheckingUser ? (
-        <>
-          <nav>
-            <ul>
-              <li>
-                <Link to='/' className='text-blue-500 underline cursor-pointer'>
-                  Main
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to='/friends'
-                  className='text-blue-500 underline cursor-pointer'
-                >
-                  Friends
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to='/auth/login'
-                  className='text-blue-500 underline cursor-pointer'
-                >
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to='/auth/register'
-                  className='text-blue-500 underline cursor-pointer'
-                >
-                  Register
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <Outlet />
-        </>
-      ) : (
-        'Loading'
-      )}
+      <Header />
+      <Outlet />
     </div>
   );
 };
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,44 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { ReactNode } from 'react';
+import { createBrowserRouter, Navigate, RouteProps } from 'react-router-dom';
 import App from './App';
+import { useAuth } from './features/auth/AuthProvider';
 import Conversation from './pages/Conversation';
 import Friends from './pages/Friends';
 import Login from './pages/Login';
 import Main from './pages/Main';
 import Register from './pages/Register';
+
+type ConditionalRouteProps = {
+  routeType: 'auth' | 'protected';
+  page: ReactNode;
+};
+
+const ConditionalRoute = ({
+  routeType = 'auth',
+  page,
+}: ConditionalRouteProps) => {
+  const { user, isCheckingUser } = useAuth();
+
+  if (isCheckingUser) {
+    return <p>Loading Wait mtfk</p>;
+  }
+
+  return (
+    <>
+      {routeType === 'auth' ? (
+        user ? (
+          <Navigate to='/' />
+        ) : (
+          page
+        )
+      ) : user ? (
+        page
+      ) : (
+        <Navigate to='/auth/login' />
+      )}
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
@@ -13,23 +47,25 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Main />,
+        element: <ConditionalRoute page={<Main />} routeType='protected' />,
       },
       {
         path: 'conversations/:friendId',
-        element: <Conversation />,
+        element: (
+          <ConditionalRoute page={<Conversation />} routeType='protected' />
+        ),
       },
       {
         path: 'friends',
-        element: <Friends />,
+        element: <ConditionalRoute page={<Friends />} routeType='protected' />,
       },
       {
         path: 'auth/register',
-        element: <Register />,
+        element: <ConditionalRoute page={<Register />} routeType='auth' />,
       },
       {
         path: 'auth/login',
-        element: <Login />,
+        element: <ConditionalRoute page={<Login />} routeType='auth' />,
       },
     ],
   },
